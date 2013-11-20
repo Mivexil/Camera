86ea541d2a76b1764c3f045cb011d8dcb94ab5f5
Mivexil <maciejas_jmj@o2.pl>
2013-11-20 22:42:46 +0100

    First commit

---
+++ CameraTestII/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CameraTestII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxR = new System.Windows.Forms.TextBox();
+            this.textBoxG = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxH = new System.Windows.Forms.TextBox();
+            this.textBoxS = new System.Windows.Forms.TextBox();
+            this.textBoxL = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(719, 588);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(737, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(719, 588);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(737, 627);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(633, 45);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1376, 627);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBoxR
+            // 
+            this.textBoxR.Location = new System.Drawing.Point(13, 626);
+            this.textBoxR.Name = "textBoxR";
+            this.textBoxR.Size = new System.Drawing.Size(100, 20);
+            this.textBoxR.TabIndex = 3;
+            // 
+            // textBoxG
+            // 
+            this.textBoxG.Location = new System.Drawing.Point(119, 627);
+            this.textBoxG.Name = "textBoxG";
+            this.textBoxG.Size = new System.Drawing.Size(100, 20);
+            this.textBoxG.TabIndex = 3;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(225, 627);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxB.TabIndex = 3;
+            // 
+            // textBoxH
+            // 
+            this.textBoxH.Location = new System.Drawing.Point(417, 627);
+            this.textBoxH.Name = "textBoxH";
+            this.textBoxH.Size = new System.Drawing.Size(100, 20);
+            this.textBoxH.TabIndex = 3;
+            // 
+            // textBoxS
+            // 
+            this.textBoxS.Location = new System.Drawing.Point(523, 628);
+            this.textBoxS.Name = "textBoxS";
+            this.textBoxS.Size = new System.Drawing.Size(100, 20);
+            this.textBoxS.TabIndex = 3;
+            // 
+            // textBoxL
+            // 
+            this.textBoxL.Location = new System.Drawing.Point(629, 628);
+            this.textBoxL.Name = "textBoxL";
+            this.textBoxL.Size = new System.Drawing.Size(100, 20);
+            this.textBoxL.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1473, 684);
+            this.Controls.Add(this.textBoxL);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxS);
+            this.Controls.Add(this.textBoxG);
+            this.Controls.Add(this.textBoxH);
+            this.Controls.Add(this.textBoxR);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxR;
+        private System.Windows.Forms.TextBox textBoxG;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxH;
+        private System.Windows.Forms.TextBox textBoxS;
+        private System.Windows.Forms.TextBox textBoxL;
+    }
+}
+
---
+++ CameraTestII/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.VisualStyles;
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision;
+using AForge.Vision.Motion;
+using NAudio.Wave;
+
+namespace CameraTestII
+{
+    
+    public partial class Form1 : Form
+    {
+        private VideoCaptureDevice _camera;
+        private delegate void SetTextCallback(string text, ref TextBox box);
+        private readonly HSLFiltering _filterHsl = new HSLFiltering(new IntRange(170, 230), new Range(0.8f, 1), new Range(0, 0.9f));
+        private readonly Grayscale _filterGrayscale = Grayscale.CommonAlgorithms.BT709;
+        private readonly Erosion3x3 _filterErosion = new Erosion3x3();
+        private SineProvider sine;
+        private WaveOut wout;
+        private BlobCounter _blob = new BlobCounter();
+        private int frequency = 0;
+        private int volume = 0;
+        private void setText(string text, ref TextBox box)
+        {
+            if (box.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(setText);
+                try
+                {
+                    Invoke(d, new object[] {text, box});
+                }
+                catch (ObjectDisposedException)
+                {
+                }
+            }
+            else
+            {
+                box.Text = text;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            _camera = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            _camera.NewFrame += new NewFrameEventHandler(NewFrameHandler);
+            _camera.Start();
+            textBox1.Text = trackBar1.Value.ToString();
+            sine = new SineProvider();
+            sine.SetWaveFormat(16000, 1);
+            wout = new WaveOut();
+            wout.Init(sine);
+            wout.Play();
+        }
+
+        private void NewFrameHandler(object sender, NewFrameEventArgs e)
+        {
+            Bitmap frame = new Bitmap(e.Frame);
+            UnmanagedImage unmanaged = UnmanagedImage.FromManagedImage(frame);
+            Color c = Color.FromArgb(255, 255, 255);
+            for (int i = -2; i <= 2; i++)
+            {
+                for (int j = -2; j <= 2; j++)
+                {
+                    unmanaged.SetPixel(100+i, 100+j, c);
+                }
+            }
+            Color c2 = frame.GetPixel(100, 100);
+            setText(c2.R.ToString(), ref textBoxR);
+            setText(c2.G.ToString(), ref textBoxG);
+            setText(c2.B.ToString(), ref textBoxB);
+            HSL hsl = HSL.FromRGB(new RGB(c2));
+            setText(hsl.Hue.ToString(), ref textBoxH);
+            setText(hsl.Saturation.ToString(), ref textBoxS);
+            setText(hsl.Luminance.ToString(), ref textBoxL);
+            _filterHsl.ApplyInPlace(unmanaged);
+            UnmanagedImage unmanaged2 = _filterGrayscale.Apply(unmanaged);
+            _filterErosion.ApplyInPlace(unmanaged2);
+            _filterErosion.ApplyInPlace(unmanaged2);
+            _blob.ProcessImage(unmanaged2);
+            Rectangle[] rects = _blob.GetObjectsRectangles();
+            int xCent = -1, yCent = -1;
+            if (rects.Length != 0)
+            {
+                Rectangle r = rects.Aggregate((r1, r2) => (r1.Height*r1.Width) > (r2.Height*r2.Width) ? r1 : r2);
+                xCent = r.Left + r.Width/2;
+                yCent = r.Top + r.Height/2;
+            }
+            if (xCent >= 2 && yCent >= 2 && xCent <= unmanaged.Width-2 && yCent <= unmanaged.Height-2)
+            {
+                for (int i = -2; i <= 2; i++)
+                {
+                    for (int j = -2; j <= 2; j++)
+                    {
+                        unmanaged2.SetPixel(xCent + i, yCent + j, 255);
+                    }
+                }
+            }
+            pictureBox2.Image = unmanaged2.ToManagedImage();
+            pictureBox1.Image = frame;
+            sine.Amplitude = 1 - yCent/960.0f;
+            sine.Frequency = 440 + xCent;
+        }
+    }
+    public class SineProvider : WaveProvider32
+    {
+        private int sample;
+        public float Frequency { get; set; }
+        public float Amplitude { get; set; }
+        public override int Read(float[] buffer, int offset, int sampleCount)
+        {
+            int sampleRate = WaveFormat.SampleRate;
+            for (int n = 0; n < sampleCount; n++)
+            {
+                buffer[n + offset] = (float)(Amplitude * Math.Sin((2 * Math.PI * sample * Frequency) / sampleRate));
+                sample++;
+                if (sample >= sampleRate) sample = 0;
+            }
+            
+   
+            return sampleCount;
+        }
+    }
+}
